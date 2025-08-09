c4d8a98a556976117e8ef5a7600e340a7f305af6
evgen-kaz <itroute@mail.ru>
2025-08-09 21:48:45 +0800

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager"; //ждем только DOMContentLoaded
+        Configuration.holdBrowserOpen = true; //оставляем браузер открытым
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $x("//*[@id='userName']").setValue("Anna");
+        $x("//*[@id='userEmail']").setValue("Anna@testova.ru");
+        $x("//*[@id='currentAddress']").setValue("street of Roses 51");
+        $x("//*[@id='permanentAddress']").setValue("street of Soviet 29");
+        $x("//*[@id='submit']").click();
+
+        $x("//*[@id='output']//*[@id='name']").shouldHave(text("Anna"));
+        $x("//*[@id='output']//*[@id='email']").shouldHave(text("Anna@testova.ru"));
+        $x("//*[@id='output']//*[@id='currentAddress']").shouldHave(text("street of Roses 51"));
+        $x("//*[@id='output']//*[@id='permanentAddress']").shouldHave(text("street of Soviet 29"));
+    }
+}
